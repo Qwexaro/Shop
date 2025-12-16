138d9a2bcd1cadba7ba1365439011f4141d2546a
Qwexaro <Sergikstar25.gold@mail.ru>
2025-12-16 15:12:23 +0300

    Добавьте файлы проекта.

---
+++ Shop/Controllers/ContactsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if(ModelState.IsValid) return Redirect("/");
+
+            return View("Index");
+        }
+    }
+}
---
+++ Shop/Models/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Введите имя")]
+        [Required(ErrorMessage = "Необходимо ввести имя")]
+        public required string Name { get; set; }
+        
+
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Необходимо ввести фамилию")]
+        public required string Surname { get; set; }
+
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Необходимо ввести возраст")]
+        public int Age { get; set; }
+
+
+        [Display(Name = "Введите почту")]
+        [Required(ErrorMessage = "Необходимо ввести почту")]
+        public required string Email { get; set; }
+
+
+        [Display(Name = "Введите сообщение")]
+        [Required(ErrorMessage = "Необходимо ввести сообщение")]
+        [StringLength(500, ErrorMessage = "Длина сообщения должна быть не более 500 символов")]
+        public required string Message { get; set; }
+    }
+}
