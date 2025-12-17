4d27988eb1b1c5c8327372df3206b5e497c40520
Qwexaro <Sergikstar25.gold@mail.ru>
2025-12-17 13:57:44 +0300

    добавлен логин

---
+++ Shop/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Check(Login login)
+        {
+            if (ModelState.IsValid) return Redirect("/");
+
+            return View("Index");
+        }
+    }
+}
---
+++ Shop/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class Login
+    {
+        [Display(Name = "Ввведите свой юзернейм")]
+        [Required(ErrorMessage = "Необходимо ввести юзернейм")]
+        public required string Username { get; set; }
+
+
+        [Display(Name = "Введите пароль")]
+        [Required(ErrorMessage = "Необходимо ввести свой пароль")]
+        public required string Password { get; set; }
+    }
+}
